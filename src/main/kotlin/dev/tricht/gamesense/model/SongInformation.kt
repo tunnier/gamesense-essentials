42ba80fad4331ba4cdb8cb6d7371dd4a08769f1a
tunnier <71137669+tunnier@users.noreply.github.com>
2022-10-27 21:08:23 -0400

    Update SongInformation.kt

--- src/main/kotlin/dev/tricht/gamesense/model/SongInformation.kt
+++ src/main/kotlin/dev/tricht/gamesense/model/SongInformation.kt
@@ -3,8 +3,18 @@ package dev.tricht.gamesense.model
 data class SongInformation(
     val fullSongName: String
 ) {
-    private val artistText = ScrollingText(fullSongName.split(" - ")[0])
-    private val songText = ScrollingText(fullSongName.split(" - ")[1])
+    if (length(fullSongName.split(" - ")[0]) >= 12) {
+        private val artistText = ScrollingText(fullSongName.split(" - ")[0] + "      ")
+    }
+    else {
+        private val artistText = ScrollingText(fullSongName.split(" - ")[0])
+    }
+    if (length(fullSongName.split(" - ")[1]) >= 12) {
+        private val songText = ScrollingText(fullSongName.split(" - ")[1] + "        ")
+    }
+    else {
+        private val songText = ScrollingText(fullSongName.split(" - ")[1])
+    }
     fun artist() = artistText.text
     fun song() = songText.text
 }
